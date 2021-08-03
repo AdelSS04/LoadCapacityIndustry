785ebf847a214807a2d4d557d7c554de2e40ddcb
Adel Lajil <adel.lajil@podyam.com>
2021-08-03 15:07:03 +0100

    fix project

--- Charge Capa/BEL/BesoinM.cs
+++ Charge Capa/BEL/BesoinM.cs
@@ -8,10 +8,6 @@ namespace BEL
 {
     public class BesoinM : Besoin
     {
-        public int WeekWork { get; set; }
-        public double AcutalCapa { get; set; }
-        public double ActualCharge { get; set; }
-        public double BesoinH { get; set; }
         public int Shift { get; set; }
         public double OpenDay { get; set; }
     }
--- Charge Capa/BEL/BesoinOP.cs
+++ Charge Capa/BEL/BesoinOP.cs
@@ -8,10 +8,6 @@ namespace BEL
 {
     public class BesoinOP : Besoin
     {
-        public int WeekWork { get; set; }
-        public double AcutalCapa { get; set; }
-        public double ActualCharge { get; set; }
-        public double BesoinH { get; set; }
         public int NbrOp { get; set; }
         public double OpNeed { get; set; }
     }
